fbc3c92a52d172301a99ba4b67bf271f245db4be
Minh Nguyen <nhtminh2999@gmail.com>
2022-03-21 16:38:19 +0700

    update font awesome

--- pages/_document.js
+++ pages/_document.js
@@ -6,9 +6,6 @@ export default class Document extends NextDocument {
       <Html lang="en" className='scroll-smooth'>
         <Head>
           <link rel='icon' href='/favicon.png'></link>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
-            integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ=="
-            crossOrigin="anonymous" referrerPolicy="no-referrer" />
           <link href="https://cdn.staticaly.com/gh/hung1001/font-awesome-pro/4cac1a6/css/all.css" rel="stylesheet"
             type="text/css" referrerPolicy="no-referrer" />
         </Head>
